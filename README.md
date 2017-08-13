334b34742385b168ed4770bfdd77586cdb5ff875
FisherBi <1015329590@qq.com>
2017-08-13 17:33:48 +0800

    create project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data structures and algorithm analysis
+
+Source code of data structures and algorithm
---
+++ src/algorithms/stackandqueue/设计一个有getMin功能的栈/MyStack.java
@@ -0,0 +1,10 @@
+package algorithms.stackandqueue.设计一个有getMin功能的栈;
+
+/**
+ * Created by FisherBi on 2017/8/13.
+ */
+public class MyStack {
+    public static void main(String args[]){
+        System.out.println("aaa");
+    }
+}
